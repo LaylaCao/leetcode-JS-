9c4e078add97978c28c4998e6c660e5b528501f4
lovelybaby <caojiali1995@gmail.com>
2019-02-12 14:40:26 +0800

    add annotation

--- addTwoNumbers/addTwoNumbers.js
+++ addTwoNumbers/addTwoNumbers.js
@@ -1,11 +1,14 @@
 /**
- * https://leetcode.com/problems/two-sum/description/
- * Difficulty:Easy
- *
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * Example:
- * Given nums = [2, 7, 11, 15], target = 9,
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
- */
+
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ * 示例：
+
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ **/
