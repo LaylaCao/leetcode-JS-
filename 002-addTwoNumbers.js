14d36def29c1c77c30dc62c1578fd637429ce67c
caojiali <caojiali1995@gmail.com>
2019-06-12 11:43:20 +0800

    Update

--- 002-addTwoNumbers.js
+++ 002-addTwoNumbers.js
@@ -57,7 +57,9 @@ var addTwoNumbers = function (l1, l2) {
 
     while (l1 || l2) {
         let a = l1 ? l1.val : 0;
+        console.log(a)
         let b = l2 ? l2.val : 0;
+        console.log(b)
         let sum = a + b + c;
         c = Math.floor(sum / 10);
         curr.next = new ListNode(sum % 10);
@@ -105,3 +107,46 @@ addTwoNumbers (l1,l2) //console.log(9-8-6)
 // QA: 如果链表长度不一呢？
 
 
+// solution4:
+var addTwoNumbers = function(l1, l2) {
+    var carried = 0; // 用于进位
+    const head = new ListNode();
+    const noop = {
+      val: 0,
+      next: null
+    };
+    let currentL1 = l1;
+    let currentL2 = l2;
+    let currentNode = head; // 返回的链表的当前node
+    let newNode; // 声明在外面节省内存
+    let previousNode; // 记录前一个节点，便于删除最后一个节点
+  
+    while (currentL1 || currentL2) {
+      newNode = new ListNode(0);
+      
+      currentNode.val =
+        ((currentL1 || noop).val + (currentL2 || noop).val + carried) % 10;
+  
+      currentNode.next = newNode;
+      previousNode = currentNode;
+      currentNode = newNode;
+  
+      if ((currentL1 || noop).val + (currentL2 || noop).val + carried >= 10) {
+        carried = 1;
+      } else {
+        carried = 0;
+      }
+  
+      currentL1 = (currentL1 || noop).next;
+      currentL2 = (currentL2 || noop).next;
+    }
+  
+    if (carried) {
+      // 还有位没进呢
+      previousNode.next = new ListNode(carried)
+    } else {
+      previousNode.next = null;
+    }
+  
+    return head;
+  };
